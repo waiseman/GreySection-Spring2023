b4ac701f18072908f04fa9a6a97b9d75ac5da2d9
Test USer <testex@abc.com>
2023-03-30 23:17:19 +0500

    -form data -validations

--- GreySectionComp/Model/Student.cs
+++ GreySectionComp/Model/Student.cs
@@ -1,9 +1,16 @@
-﻿namespace GreySectionComp.Model
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GreySectionComp.Model
 {
     public class Student
     {
+        [Required]
         public int Id { get; set; }
+        [Required]
+        [StringLength(50,ErrorMessage="Input Name Limit Exceeds",MinimumLength =3)]
         public string Name { get; set; }
+
         public string RollNumber { get; set; }
+        public DateTime DOB { get; set; }
     }
 }
